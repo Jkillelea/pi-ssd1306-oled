56a12ca581fadc23746329d746420bc9e0b5ee06
Jacob Killelea <jkillelea@protonmail.ch>
2018-06-03 17:41:33 -0700

    misc

--- ssd1306.cpp
+++ ssd1306.cpp
@@ -9,10 +9,8 @@
 #endif
 
     SSD1306::SSD1306() {
-        for(int i=0; i<8; i++)
-        {
-            for(int j=0; j<128; j++)
-            {
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 128; j++) {
                 displayLines[i][j] = 0;
             }
         }
@@ -26,43 +24,31 @@
 
     void SSD1306::textDisplay(const char *message) {
         if(!i2cInitialised)
-        {
             initDisplay();
-        }
 
         // and scroll up if needed (in SCROLL mode)
         if((displayMode == SSD1306::Mode::SCROLL) && needScroll)
-        {
             scrollUp(1);
-        } 
 
         D printf("textDisplay - %s\n", message);
         int currByteCount = 0;
-        for(int i=0; i<strlen(message); i++)
-        {
+        for(int i=0; i<strlen(message); i++) {
             int bytesAdded = addFontBytes(currByteCount, message[i]);
-            if(bytesAdded > 0)
-            {
+            if(bytesAdded > 0) {
                 currByteCount += bytesAdded;
-            }
-            else
-            {
-                if(wordWrap)
-                {
+            } else {
+                if(wordWrap) {
                     // fill the rest of this line with 0s
-                    while(currByteCount < 128)
-                    {
+                    while(currByteCount < 128) {
                         displayLines[currentLine][currByteCount++] = 0x00;
                     }
                     // move it onto the next line
                     currentLine++;
                     // and scroll up if needed (in SCROLL mode)
-                    if((displayMode == SSD1306::Mode::SCROLL) && needScroll)
-                    {
+                    if((displayMode == SSD1306::Mode::SCROLL) && needScroll) {
                         scrollUp(1);
                     } 
-                    if (currentLine > 7) 
-                    { 
+                    if (currentLine > 7) { 
                         currentLine = 0;  
                         needScroll = TRUE;                  
                     }
@@ -73,16 +59,14 @@
         }
 
         // now fill up any left over with 0x00s on the current line
-        while(currByteCount < 128)
-        {
+        while(currByteCount < 128) {
             displayLines[currentLine][currByteCount++] = 0x00;
         }
 
         // now point to next line and set to first char
         currByteCount = 0;
         currentLine++;
-        if(currentLine > 7) 
-        { 
+        if(currentLine > 7) { 
             currentLine = 0;
             needScroll = TRUE;
         }
@@ -92,10 +76,8 @@
 
     void SSD1306::clearDisplay() {
         // blank out the line buffers
-        for(int i=0; i<8; i++)
-        {
-            for(int j=0; j<128; j++)
-            {
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 128; j++) {
                 displayLines[i][j] = 0;
             }
         }
@@ -117,11 +99,13 @@
     // private functions
     void SSD1306::scrollUp(int lines) {
         if(!i2cInitialised)
-        {
             initDisplay();
-        }
+
         currentScrollLine += lines;
-        if (currentScrollLine > 7) { currentScrollLine -= 8; }
+
+        if (currentScrollLine > 7) 
+            currentScrollLine -= 8;
+
         scrollUpSequence[2] = currentScrollLine * 8;
         D printf("scrollUp\n");
 		writeI2C(scrollUpSequence, 3);
@@ -132,23 +116,19 @@
         c = toupper(c); // we only support UPPERCASE letters
         int letterIdx = (c - ' ');
         if(letterIdx > 64)
-        {
             letterIdx = 65;
-        }
 
         int letterBytes = fontData[letterIdx][0];
-        if((curr + letterBytes + 1) > 127 )
-        {
+
+        if((curr + letterBytes + 1) > 127 ) {
             return 0;
             D printf("\n");
-        }
-        else
-        {
-            for(int i=0; i<letterBytes; i++)
-            {
+        } else {
+            for(int i = 0; i < letterBytes; i++) {
                 D printf("%x ", fontData[letterIdx][1 + i]);
                 displayLines[currentLine][curr + i] = fontData[letterIdx][1 + i]; 
             }
+
             displayLines[currentLine][curr + letterBytes++] = 0x00;  // single byte space / seperator
             D printf("\n");
             return letterBytes;
@@ -159,16 +139,14 @@
         // -1 = full range
         // 0..7 = line
         D printf("setDisplayRange (7 bytes)\n");
-        if(line == -1)
-        {
+        if(line == -1) {
             writeI2C(setFullRange, 7);
         }
     }
 
     void SSD1306::updateDisplayFull() {
         setDisplayRange(-1);
-        for(int line=0; line<8; line++)
-        {
+        for(int line = 0; line < 8; line++) {
             unsigned char buffer[129] = {0};
             buffer[0] = 0x40;
             for(int i=0; i<128; i++) {
@@ -180,21 +158,17 @@
 
     void SSD1306::writeI2C(unsigned char* data, int bytes) {
         char *deviceName = (char*)"/dev/i2c-1";
-	    if ((i2cHandle = open(deviceName, O_RDWR)) < 0)
-	    {
+	    if ((i2cHandle = open(deviceName, O_RDWR)) < 0) {
             printf("error opening I2C\n");
-        }
-        else
-        {
+        } else {
             D printf("Opened I2C bus (Handle = %i)\n", i2cHandle);
-            if (ioctl(i2cHandle, I2C_SLAVE, i2cAddress) < 0)
-            {
+            if (ioctl(i2cHandle, I2C_SLAVE, i2cAddress) < 0) {
                 printf("Error at ioctl\n");
-            }
-            else
-            {
+            } else {
                 D printf("writeI2C : ");
-                for(int i=0; i<bytes; i++) { D printf("%x ", data[i]); }
+                for(int i = 0; i < bytes; i++) {
+                    D printf("%x ", data[i]);
+                }
                 write(i2cHandle, data, bytes);
                 D printf("\n");
             }
