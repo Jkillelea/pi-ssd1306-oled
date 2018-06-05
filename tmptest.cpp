321eac0ca0903cb697774578b5a6338f0a423b60
Jacob Killelea <jkillelea@protonmail.ch>
2018-06-04 17:07:31 -0700

    misc

--- tmptest.cpp
+++ tmptest.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[]) {
     SSD1306 display = SSD1306();
 
-    for (char i = 0; i < 256; i <<= 1) {
+    for (char i = 0; i < 256; i = i << 1) {
         printf("%d\n", i);
         display.clear();
         display.testdraw(i);
