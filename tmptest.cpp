a40d277eb25d2233e9e55bca2c931df85f86560c
Jacob Killelea <jkillelea@protonmail.ch>
2018-06-07 16:23:45 -0700

    misc

--- tmptest.cpp
+++ tmptest.cpp
@@ -4,6 +4,9 @@ int main(int argc, char const *argv[]) {
     SSD1306 display = SSD1306();
 
     display.clear();
+    for (char ch = ' '; ch <= 'Z'; ch++) {
+        display.putc(ch);
+    }
     display.testdraw();
     // display.every_pixel();
 
