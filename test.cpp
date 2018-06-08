87f23046094a23b5ab2e73c792c613a6d868bf21
Jacob Killelea <jkillelea@protonmail.ch>
2018-06-08 11:43:47 -0700

    misc

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[]) {
     SSD1306 display = SSD1306();
 
     display.clear();
-    display.print((char *)msg);
+    display.print("potato");
     // for (char ch = ' '; ch <= '`'; ch++) {
     //     display.putc(ch);
     // }
